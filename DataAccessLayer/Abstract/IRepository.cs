a58a13b2e15cfc7e328c05e9c0ca0b2b5f48f511
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-06 23:54:08 +0300

    IRespository updated.

--- DataAccessLayer/Abstract/IRepository.cs
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace DataAccessLayer.Abstract
 {
     public interface IRepository<T>
     {
-        List<T> List();
+        List<T> List(Expression<Func<T,bool>> filter=null);
         void Insert(T entity);
         void Delete(T entity);
         void Update(T entity);
