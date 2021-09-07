0460653d8150c75c816c54b2114e0c99d5b97f0d
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-07 22:22:03 +0300

    error fix and add category

--- BusinessLayer/ValidationRules/CategoryValidator.cs
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -14,7 +14,8 @@ namespace BusinessLayer.ValidationRules
         {
             RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adı boş geçilemez.");
             RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklama boş bırakılamaz.");
-            RuleFor(x => x.CategoryName).MinimumLength(30).MaximumLength(20).WithMessage("Kategori adı 3 - 20 karakter içermelidir.");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Kategori adı en az 3 içermelidir.");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Kategori adı en fazla 20 karakter içermelidir.");
         }
     }
 }
