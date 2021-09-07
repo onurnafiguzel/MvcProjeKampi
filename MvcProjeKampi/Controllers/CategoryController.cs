b43b17a2f8d07e7471ad67c4d5aa7b99a8b23ad2
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-07 18:14:39 +0300

    CategoryController List function

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,18 @@ namespace MvcProjeKampi.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+
+        CategoryManager categoryManager = new CategoryManager();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetList()
+        {
+            var result = categoryManager.GetAll();
+            return View(result);
         }
     }
 }
