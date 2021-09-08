dfa1662f4689d84e90ee3559e0fda0735a7ccb9e
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-08 17:08:31 +0300

    CategoryAdd function

--- MvcProjeKampi/Controllers/AdminCategoryController.cs
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +18,34 @@ namespace MvcProjeKampi.Controllers
         public ActionResult Index()
         {
             var resut = categoryManager.GetAll();
-            return View(resut) ;
+            return View(resut);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(category);
+            if (result.IsValid)
+            {
+                categoryManager.Add(category);
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
