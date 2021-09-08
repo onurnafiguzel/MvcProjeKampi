3dc0b62063e7beae210b260c42b46ddb82f0d14b
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-08 23:04:07 +0300

    Writer validaiton

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı boş geçilemez.");
+            RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadı boş bırakılamaz.");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkımda boş bırakılamaz.");
+            RuleFor(x => x.WriterSurName).MinimumLength(2).WithMessage("Yazar soyadı en az 2 içermelidir.");
+            RuleFor(x => x.WriterSurName).MaximumLength(20).WithMessage("Yazar soyadı en fazla 20 karakter içermelidir.");
+           // RuleFor(x => x.WriterSurName).Must(StartWithA).WithMessage("Yazar soyadı 'A'  harfi içermelidir.");
+        }
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
