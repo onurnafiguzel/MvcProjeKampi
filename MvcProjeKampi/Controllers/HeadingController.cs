47b4f7c0c23a3234bb7316ecfc5b1f95fb0064dc
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-09 18:01:30 +0300

    Headings Listed

---
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+
+        public ActionResult Index()
+        {
+            var result = headingManager.GetAll();
+            return View(result);
+        }
+    }
+}
