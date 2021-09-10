3f43043cecd1c63da7feab16677849a09585966c
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-10 16:28:26 +0300

    ContentStatus added

--- EntityLayer/Concrete/Content.cs
+++ EntityLayer/Concrete/Content.cs
@@ -13,8 +13,8 @@ namespace EntityLayer.Concrete
         public int ContentId { get; set; }
         [StringLength(1000)]
         public string ContentValue { get; set; }
-
         public DateTime ContentDate { get; set; }
+        public bool ContentStatus { get; set; }
 
         public int HeadingId { get; set; }
         public virtual Heading Heading { get; set; }
