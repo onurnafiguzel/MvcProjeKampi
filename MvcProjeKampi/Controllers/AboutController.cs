d531d55e8cb03e589b08d028fabcae3c03ed9f31
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-10 23:36:05 +0300

    AddAbout added.

--- MvcProjeKampi/Controllers/AboutController.cs
+++ MvcProjeKampi/Controllers/AboutController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,24 @@ namespace MvcProjeKampi.Controllers
     public class AboutController : Controller
     {
         // GET: About
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAbout(About about)
+        {
+            aboutManager.AboutAdd(about);
+            return RedirectToAction("Index");
         }
     }
 }
