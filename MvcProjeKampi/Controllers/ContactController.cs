5a1836e6b9f51f3a127f37e2e0113022f5d52435
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-11 16:41:34 +0300

    Messages Listed.

--- MvcProjeKampi/Controllers/ContactController.cs
+++ MvcProjeKampi/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,13 @@ namespace MvcProjeKampi.Controllers
     public class ContactController : Controller
     {
         // GET: Contact
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        ContactValidator contactValidator = new ContactValidator();
+
         public ActionResult Index()
         {
-            return View();
+            var result = contactManager.GetAll();
+            return View(result);
         }
     }
 }
