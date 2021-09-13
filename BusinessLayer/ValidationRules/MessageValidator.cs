9f9349a996848fa9a0a57e46e0e9fb5f3b760726
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-13 15:47:24 +0300

    MessageValitor added.

---
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class MessageValidator : AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Alıcı adresini boş geçemezsiniz.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konuyu boş geçemezsiniz.");
+            RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Mesajı boş geçemezsiniz.");
+            RuleFor(x => x.Subject).MaximumLength(100).WithMessage("Konu 100 karakteri geçemez.");
+        }
+    }
+}
