8aa6e23e0a1817adcb77a1e3c7460c9c2c934aee
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-15 15:50:55 +0300

    WriterPanelMessage InBox added.

--- MvcProjeKampi/Controllers/WriterPanelMessageController.cs
+++ MvcProjeKampi/Controllers/WriterPanelMessageController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,13 @@ namespace MvcProjeKampi.Controllers
     public class WriterPanelMessageController : Controller
     {
         // GET: WriterPanelMessage
-        public ActionResult Index()
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        MessageValidator messageValidator = new MessageValidator();
+
+        public ActionResult Inbox()
         {
-            return View();
+            var result = messageManager.GetAllInBox();
+            return View(result);
         }
     }
 }
