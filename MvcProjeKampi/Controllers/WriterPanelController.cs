225d0bc305422126c560753671dc459070da5ab3
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-15 13:48:35 +0300

    HeadingManager defined in WriterPanelController

--- MvcProjeKampi/Controllers/WriterPanelController.cs
+++ MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +11,14 @@ namespace MvcProjeKampi.Controllers
     public class WriterPanelController : Controller
     {
         // GET: WriterPanel
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+
         public ActionResult WriterProfile()
+        {
+            return View();
+        }
+
+        public ActionResult MyHeading()
         {
             return View();
         }
