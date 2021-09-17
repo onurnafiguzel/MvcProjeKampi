39716a8798ad25a7315b83fda3a06845f8d85cc2
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-17 17:30:59 +0300

    Default Controlller and Headings page added.

---
+++ MvcProjeKampi/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Headings()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
