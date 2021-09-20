19445fbee3e24d71db88db11e9e2215529fe629d
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-20 15:20:51 +0300

    LogOut funtion is maden

--- MvcProjeKampi/Controllers/LoginController.cs
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -60,5 +60,12 @@ namespace MvcProjeKampi.Controllers
                 return RedirectToAction("WriterLogin");
             }
         }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Headings", "Default");
+        }
     }
 }
