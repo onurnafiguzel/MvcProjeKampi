acda12eb4aeb5a643b1911710da52af96d6b62a8
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-20 09:12:56 +0300

    Add content a heading

--- MvcProjeKampi/Controllers/WriterPanelContentController.cs
+++ MvcProjeKampi/Controllers/WriterPanelContentController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,11 +17,29 @@ namespace MvcProjeKampi.Controllers
 
         Context context = new Context();
         public ActionResult MyContent(string parameter)
-        {          
+        {
             parameter = (string)Session["WriterMail"];
             var writeridinfo = context.Writers.Where(x => x.WriterMail == parameter).Select(y => y.WriterId).FirstOrDefault();
             var result = contentManager.GetAllByWriter(writeridinfo);
             return View(result);
+        }
+
+        [HttpGet]
+        public ActionResult AddContent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddContent(Content content)
+        {
+            string mail = (string)Session["WriterMail"];
+            var writeridinfo = context.Writers.Where(x => x.WriterMail == mail).Select(y => y.WriterId).FirstOrDefault();
+            content.ContentDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            content.WriterId = writeridinfo;
+            content.ContentStatus = true;
+            contentManager.ContentAdd(content);
+            return RedirectToAction("MyContent");
         }
     }
 }
