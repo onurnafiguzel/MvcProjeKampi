c5ef9ac94668f688f324b5f9f06318cb9ad0805c
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-20 21:40:22 +0300

    IWriterService and WriterLoginManager added.

---
+++ BusinessLayer/Concrete/WriterLoginManager.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterLoginManager : IWriterLoginService
+    {
+        public Writer GetWriter(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
