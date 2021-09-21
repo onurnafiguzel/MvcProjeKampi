d262972f3f8cd080a08baa57ff01f568f7d4b26a
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-09-21 20:23:56 +0300

    ChartController added.

---
+++ MvcProjeKampi/Controllers/ChartController.cs
@@ -0,0 +1,49 @@
+﻿using MvcProjeKampi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<Category> BlogList()
+        {
+            List<Category> categories = new List<Category>();
+            categories.Add(new Category()
+            {
+                CategoryName = "yazılım",
+                CategoryCount = 1
+            });
+            categories.Add(new Category()
+            {
+                CategoryName = "seyahat",
+                CategoryCount = 2
+            });
+            categories.Add(new Category()
+            {
+                CategoryName = "teknoloji",
+                CategoryCount = 3
+            });
+            categories.Add(new Category()
+            {
+                CategoryName = "spor",
+                CategoryCount = 4
+            });
+            return categories;
+        }
+    }
+}
